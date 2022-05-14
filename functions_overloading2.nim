c9c231369a25b33fee9ea80756bd8e7d89b8a766
Sergio Lima <sergiosouzalima@gmail.com>
2022-05-14 17:48:05 -0300

    Updating repo

--- functions_overloading2.nim
+++ functions_overloading2.nim
@@ -1,4 +1,4 @@
-# functions_overloading.nim
+# functions_overloading2.nim
 #
 # function: overloading
 
@@ -8,10 +8,7 @@ proc positiveOrNegative(num: int): string =
     if num > 0: return "positive"
 
 proc positiveOrNegative(num: float): string =
-    if num == 0: return "zero"
-    if num < 0: return "negative"
-    if num > 0: return "positive"
-
+    return positiveOrNegative(toInt(num))
 
 echo positiveOrNegative 3.14
 echo positiveOrNegative 3
